d56517f8f7a885a52ed7d5661a2de507bcb9a176
DAloG <dalog@me.com>
2015-03-28 11:54:25 +0200

    Add project
    Simple structure.

---
+++ Robe Guard/sources/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Robe Guard
+//
+//  Created by Алексей Демедецкий on 28.03.15.
+//  Copyright (c) 2015 DAloG ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
